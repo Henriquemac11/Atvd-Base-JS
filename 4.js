86f69f3c6122de6838fd44c8d81923ed3ba05b02
Henrique Maciel <164898450+Henriquemac11@users.noreply.github.com>
2026-03-12 07:42:11 -0300

    Add customized multiplication table script

---
+++ 4.js
@@ -0,0 +1,6 @@
+//Tabuada Personalizada
+let numero = Number(prompt("Digite um número para ver a tabuada:"));
+
+for (let i = 1; i <= 10; i++) {
+    console.log(numero + " x " + i + " = " + (numero * i));
+}
